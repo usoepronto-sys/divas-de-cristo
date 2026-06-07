63c35f66d5b0b53fa5207a19aeecfdc5c3c2d509
Rogerio Lima <rogeriobernardescosta@hotmail.com>
2026-06-07 01:54:43 -0300

    Ajusta busca de produtos destaque

--- loja/views.py
+++ loja/views.py
@@ -1,34 +1,15 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Produto, Categoria
+from django.shortcuts import render
+from .models import Produto
 
 
 def home(request):
-    categorias = Categoria.objects.filter(ativa=True)
-
     produtos_destaque = Produto.objects.filter(
-        destaque=True,
-        disponivel=True
+        disponivel=True,
+        destaque=True
     ).order_by('ordem', '-criado_em')
 
     contexto = {
-        'categorias': categorias,
-        'produtos': produtos_destaque,
+        'produtos_destaque': produtos_destaque,
     }
 
     return render(request, 'home.html', contexto)
-
-
-def produtos_por_categoria(request, slug):
-    categoria = get_object_or_404(Categoria, slug=slug, ativa=True)
-
-    produtos = Produto.objects.filter(
-        categoria=categoria,
-        disponivel=True
-    ).order_by('ordem', '-criado_em')
-
-    contexto = {
-        'categoria': categoria,
-        'produtos': produtos,
-    }
-
-    return render(request, 'categoria.html', contexto)

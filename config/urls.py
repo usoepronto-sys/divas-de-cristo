3d5c259338fb390e91bb1f78d558bb468a0074cc
Rogerio Lima <rogeriobernardescosta@hotmail.com>
2026-06-07 01:31:30 -0300

    Corrige produtos destaque na home

--- config/urls.py
+++ config/urls.py
@@ -9,5 +9,4 @@ urlpatterns = [
     path('', include('loja.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

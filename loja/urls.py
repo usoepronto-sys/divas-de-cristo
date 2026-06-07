5c8d0112b2b1eb4035c7a0d32cacd7290b6ab32e
Rogerio Lima <rogeriobernardescosta@hotmail.com>
2026-06-07 01:44:35 -0300

    Adiciona paginas de categoria e media

--- loja/urls.py
+++ loja/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('colecao/<slug:slug>/', views.produtos_por_categoria, name='produtos_por_categoria'),
 ]
--- loja/views.py
+++ loja/views.py
@@ -1,21 +1,34 @@
-from django.shortcuts import render
-from .models import Produto
+from django.shortcuts import render, get_object_or_404
+from .models import Produto, Categoria
 
 
 def home(request):
+    categorias = Categoria.objects.filter(ativa=True)
 
     produtos_destaque = Produto.objects.filter(
         destaque=True,
         disponivel=True
-    )
-
-    produtos = Produto.objects.filter(
-        disponivel=True
-    )
+    ).order_by('ordem', '-criado_em')
 
     contexto = {
-        'produtos_destaque': produtos_destaque,
-        'produtos': produtos
+        'categorias': categorias,
+        'produtos': produtos_destaque,
     }
 
     return render(request, 'home.html', contexto)
+
+
+def produtos_por_categoria(request, slug):
+    categoria = get_object_or_404(Categoria, slug=slug, ativa=True)
+
+    produtos = Produto.objects.filter(
+        categoria=categoria,
+        disponivel=True
+    ).order_by('ordem', '-criado_em')
+
+    contexto = {
+        'categoria': categoria,
+        'produtos': produtos,
+    }
+
+    return render(request, 'categoria.html', contexto)

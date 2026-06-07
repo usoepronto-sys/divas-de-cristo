44c704841635a602de9cf9431a7fcdc63d37b31d
Rogerio Lima <rogeriobernardescosta@hotmail.com>
2026-06-07 04:01:25 -0300

    Corrige home removendo filtros de campos opcionais

--- loja/views.py
+++ loja/views.py
@@ -3,16 +3,16 @@ from .models import Produto, Categoria
 
 
 def home(request):
-    categorias = Categoria.objects.filter(ativa=True)
+    categorias = Categoria.objects.all()
 
     produtos_destaque = Produto.objects.filter(
         destaque=True,
         disponivel=True
-    ).order_by('ordem', '-criado_em')
+    ).order_by('-criado_em')
 
     produtos = Produto.objects.filter(
         disponivel=True
-    ).order_by('ordem', '-criado_em')
+    ).order_by('-criado_em')
 
     contexto = {
         'categorias': categorias,
@@ -24,12 +24,12 @@ def home(request):
 
 
 def produtos_por_categoria(request, slug):
-    categoria = get_object_or_404(Categoria, slug=slug, ativa=True)
+    categoria = get_object_or_404(Categoria, slug=slug)
 
     produtos = Produto.objects.filter(
         categoria=categoria,
         disponivel=True
-    ).order_by('ordem', '-criado_em')
+    ).order_by('-criado_em')
 
     contexto = {
         'categoria': categoria,

126674c358e8d585ee545b66fd0fc7ab8bf39475
Rogerio Lima <rogeriobernardescosta@hotmail.com>
2026-06-08 16:31:30 -0300

    Corrige conexao SSL com Neon

--- config/settings.py
+++ config/settings.py
@@ -69,8 +69,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASE_URL = os.environ.get('DATABASE_URL')
 
 if DATABASE_URL:
+    
     DATABASES = {
-        'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600)
+        'default': dj_database_url.parse(
+            DATABASE_URL,
+            conn_max_age=0,
+            ssl_require=True
+        )
     }
 else:
     DATABASES = {
